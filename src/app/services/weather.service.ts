e3778609ea29bf741df52efe0c4b36142941f8c8
dhruvtrip <dhruvtripathi1995@gmail.com>
2022-06-09 23:45:54 +0200

    add method to get weather data via weather.service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +9,14 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
 
   getWeatherData(cityName: string) {
-    this.http.get();
+    this.http.get(environment.weatherApiBaseUrl, {
+      headers: new HttpHeaders()
+        .set(environment.XRapidAPIHostLabel, environment.XRapidAPIHostValue)
+        .set(environment.XRapidAPIKeyLabel, environment.XRapidAPIKeyValue),
+      params: new HttpParams()
+        .set('q', cityName)
+        .set('units', 'metric')
+        .set('mode', 'json'),
+    });
   }
 }
