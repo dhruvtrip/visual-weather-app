32030bdc65a33baf2ea7d6ee1e3e7acf7b134958
dhruvtrip <dhruvtripathi1995@gmail.com>
2022-06-11 22:38:42 +0200

    subscribe to weather data service and store data in response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'visual-weather-app';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+  weatherData?: WeatherData;
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('London').subscribe({
+      next: (response) => {
+        this.weatherData = response;
+        //console.log(response);
+      },
+    });
+    throw new Error('Method not implemented.');
+  }
 }
